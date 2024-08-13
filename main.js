585361cceb28586cf8d8163f13b64556267c72b1
qolbiy <139425812+qolbiy@users.noreply.github.com>
2024-08-14 02:22:35 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let layer_1 = document.querySelector('.layer-1');
+let layer_2 = document.querySelector('.layer-2');
+let layer_3 = document.querySelector('.layer-3');
+let layer_4 = document.querySelector('.layer-4');
+let layer_5 = document.querySelector('.layer-5');
+
+window.onscroll = function(){
+    let Y = window.scrollY;
+
+    layer_1.style.transform = 'translateY('+ Y/1.5 + 'px)';
+    layer_2.style.transform = 'translateY('+ Y/2 + 'px)';
+    layer_3.style.transform = 'translateY('+ Y/1.5 + 'px)';
+    layer_4.style.transform = 'translateY('+ Y/2 + 'px)';
+    layer_5.style.transform = 'translateY('+ Y/3 + 'px)';
+
+};
